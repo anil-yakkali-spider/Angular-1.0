8e9ef580a57f5e8553b181976d3cc35325cffde2
Anil Kumar Yakkali <ayakkali@spiderlogic.com>
2017-06-22 17:39:38 +0530

    Intial commit

---
+++ Controllers/controller1.js
@@ -0,0 +1,23 @@
+﻿//var app = angular.module('myApp', []).
+//           controller('simpleController', function ($scope) {
+//               $scope.customers = [
+//                   { name: "Dave Jones", city: "Phoenix" }
+//                   , { name: "Jamie Riley", city: "Atlanta" }
+//                   , { name: "Heedy Walhin", city: "Chandler" }
+//                   , { name: "Thomas Winter", city: "Seattle" }
+//                    , { name: "Radha K", city: "Bangalor" }
+//               ];
+//           });
+
+var myApp = angular.module('myApp', []);
+var controllers = {};
+controllers.SimpleController = function ($scope) {
+    $scope.customers = [
+                   { name: "Dave Jones", city: "Phoenix" }
+                   , { name: "Jamie Riley", city: "Atlanta" }
+                   , { name: "Heedy Walhin", city: "Chandler" }
+                   , { name: "Thomas Winter", city: "Seattle" }
+                    , { name: "Radha K", city: "Bangalor" }
+               ];
+}
+myApp.controller(controllers);
---
+++ Controllers/controller2.js
@@ -0,0 +1,38 @@
+﻿var myApp = angular.module('myAppWithRoutes', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+        $routeProvider
+            .when('/',
+                {
+                    controller: SimpleController ,
+                    templateUrl:'Partials/View1.html'
+                }
+            )
+            .when('/view2',
+                {
+                    controller: SimpleController,
+                    templateUrl: 'Partials/View2.html'
+                }
+            )
+            .otherwise({redirectTo:'/'});
+
+    }
+);
+var controllers = {};
+
+controllers.SimpleController = function ($scope) {
+    $scope.customers = [
+                   { name: "Dave Jones", city: "Phoenix" }
+                   , { name: "Jamie Riley", city: "Atlanta" }
+                   , { name: "Heedy Walhin", city: "Chandler" }
+                   , { name: "Thomas Winter", city: "Seattle" }
+                    , { name: "Radha K", city: "Bangalor" }
+    ];
+    $scope.addCustomer = function() {
+        $scope.customers.push({
+            name: $scope.newCustomer.name,
+            city: $scope.newCustomer.city
+        });
+    };
+}
+myApp.controller(controllers);
